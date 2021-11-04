e54fae5b83c7c90e29b4be3fdeb0f9f736d3e473
Joseph Dillon <joseph.dillon.522@gmail.com>
2021-11-04 17:09:28 -0400

    stuff. ugh im bored

--- C_For_Beginners/castSize.c
+++ C_For_Beginners/castSize.c
@@ -8,6 +8,8 @@ int main()
 
     printf("Casting %f + %f to int %d\n", a, b, c);
     printf("Size of:\nint = %ld\nfloat = %ld\ndouble = %ld\n", sizeof(int), sizeof(float), sizeof(double));
+    printf("Size of:\nchar = %ld\nlong = %ld\nlong long = %ld\n", sizeof(char), sizeof(long), sizeof(long long));
+
 
     return 0;
 }
--- C_For_Beginners/minInYear_challenge.c
+++ C_For_Beginners/minInYear_challenge.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    double minutesInDay = 24 * 60;
-    double minutesInYear = 365 * minutesInDay;
+    double minutesInDay = 24.0 * 60.0;
+    double minutesInYear = 365.0 * minutesInDay;
 
     int inputMin;
 
@@ -13,8 +13,9 @@ int main()
     double minOfDay = minutesInDay / (double) inputMin;
     double minOfYear = minutesInYear / (double) inputMin;
 
-    printf("\n%d is 1/%dth of a day\n", inputMin, (int) minOfDay);
-    printf("\n%d is 1/%dth of a year\n", inputMin, (int) minOfYear);
+    // Not a good way to do this. yuck
+    printf("\n%d is 1/%dth of a day or %3f%% of a day\n", inputMin, (int) minOfDay, (double) inputMin / minutesInDay);
+    printf("\n%d is 1/%dth of a year or %3f%% of a year\n", inputMin, (int) minOfYear, (double) inputMin / minutesInYear);
 
     return 0;
 }
